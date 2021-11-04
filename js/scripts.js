fb453685a59be75fce13571933ef5187a615ec5d
Jack <skelton.jt9@gmail.com>
2021-11-03 23:40:50 -0400

    Added functions
    Co-authored-by: Matthew Banse-Fay <matthew.bansefay@gmail.com>
    Co-authored-by: Coy Griffin <CoyGriffi@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,40 @@
 $(document).ready(function() {
+  $("#user-height").submit(function(event) {
+    event.preventDefault();
+    let userHeight = parseInt($("#height").val());
+    const teaHeight = 36;
+    const terrorHeight = 58;
+    const flamingHeight = 60;
+    const mountainHeight = 62;
+    const maxHeightTea = 60;
+    const maxHeightFlaming = 70;
+    const maxHeightTerror = 80;
+    $("#tea-cups, #tower-of-terror, #flaming-roller-coaster, #magic-mountain").removeClass("rideable");
+    $("#tea-cups p, #tower-of-terror p, #flaming-roller-coaster p").remove();
 
+    if (userHeight >= teaHeight) {
+      $("#tea-cups").addClass("rideable");
+      if (userHeight > maxHeightTea){
+        $("#tea-cups").append("<p>You are too tall</p>")
+      }
+    } 
+    
+    if (userHeight >= terrorHeight) {
+      $("#tower-of-terror").addClass("rideable");
+      if (userHeight > maxHeightTerror){
+        $("#tower-of-terror").append("<p>You are too tall</p>")
+      }
+    }
+    
+    if (userHeight >= flamingHeight) {
+      $("#flaming-roller-coaster").addClass("rideable");
+      if (userHeight > maxHeightFlaming){
+        $("#flaming-roller-coaster").append("<p>You are too tall</p>")
+      }
+    }
+
+    if (userHeight >= mountainHeight) {
+      $("#magic-mountain").addClass("rideable");
+    }
+  });
 });
